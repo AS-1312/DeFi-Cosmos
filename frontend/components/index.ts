cbefeacef11fa0dab19c1ba771a9cf9dd763b6c5
AS-1312 <59477486+AS-1312@users.noreply.github.com>
2025-10-25 16:33:23 +0530

    chore(frontend): add components barrel, remove useWhaleActivity, enforce strict TS/ESLint build checks

---
+++ frontend/components/index.ts
@@ -0,0 +1,11 @@
+// Main components
+export { Header } from './Header';
+export { SideNav } from './SideNav';
+export { MobileNav } from './MobileNav';
+export { BottomTicker } from './BottomTicker';
+export { LiveActivityFeed } from './LiveActivityFeed';
+export { ProtocolStatsGrid } from './ProtocolStatsGrid';
+export { ProtocolHealthPanel } from './ProtocolHealthPanel';
+export { UserActivityHeatmap } from './UserActivityHeatmap';
+export { WelcomeScreen } from './WelcomeScreen';
+export { CosmosVisualization } from './CosmosVisualization';
